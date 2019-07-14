65149cf8f3b8815ee047c2486513e8770b8a1e69
enkilleisvart <bjorn.ohlsson.93@gmail.com>
2019-07-14 20:57:15 +0200

    Initial stuff. Working on register page. Added connection to Firebase database, removed index.css

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class Register extends React.Component {
   render() {
-    return <div>Register</div>;
+    return (
+    	<Grid textAlign="center" verticalAlign="middle">
+    		<Grid.Column style={{ maxwidth: 450}}>
+    			<Header as="h2" icon color="orange">
+    					Register for Bearchat
+    			</Header>
+    			<Form size="large">
+    				<Segment stacked>
+    					<Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={this.handleChange} type="text" />
+
+    					<Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email Address" onChange={this.handleChange} type="email" />
+
+    					<Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={this.handleChange} type="password" />
+
+    					<Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password Confirmation" onChange={this.handleChange} type="password" />
+
+    					<Button color="orange" fluid size="large">Register</Button>
+    				</Segment>
+    			</Form>
+    			<Message>Already a user? <Link to="/Login">Login</Link></Message>
+    		</Grid.Column>
+    	</Grid>
+    );
   }
 }
 
---
+++ src/components/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+/*<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.3.0/firebase-app.js"></script>*/
+
+/*<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->*/
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCsOHr1Y2l35wSYt_Zg8vE7inO_9O30p1c",
+    authDomain: "bearchat-app.firebaseapp.com",
+    databaseURL: "https://bearchat-app.firebaseio.com",
+    projectId: "bearchat-app",
+    storageBucket: "",
+    messagingSenderId: "465344246644",
+    appId: "1:465344246644:web:4622decaba1f840b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
+
+export default firebase;
