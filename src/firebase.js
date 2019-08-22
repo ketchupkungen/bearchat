ffa1ab5e63f4ee1328771912875394c308204337
Björn O <bjorn.ohlsson.93@gmail.com>
2019-08-22 12:10:53 +0200

    Updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/database";
 import "firebase/storage";
 
 var config = {
-  apiKey: "AIzaSyCsOHr1Y2l35wSYt_Zg8vE7inO_9O30p1c",
-    authDomain: "bearchat-app.firebaseapp.com",
-    databaseURL: "https://bearchat-app.firebaseio.com",
-    projectId: "bearchat-app",
-    storageBucket: "bearchat-app.appspot.com",
-    messagingSenderId: "465344246644",
-    appId: "1:465344246644:web:4622decaba1f840b"
+  apiKey: "#########################",
+    authDomain: "#########################",
+    databaseURL: "#########################",
+    projectId: "#########################",
+    storageBucket: "#########################",
+    messagingSenderId: "#########################",
+    appId: "#########################"
 };
 firebase.initializeApp(config);
 
